69a2ad08cfea72bed68aed12c46e39395b2ddced
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-17 04:45:20 +0300

    Create macchanger.py

---
+++ macchanger.py
@@ -0,0 +1,30 @@
+import subprocess
+import optparse
+
+print("""_   _      _ _       _ _
+| | | | ___| | | ___ | | |
+| |_| |/ _ \ | |/ _ \| | |
+|  _  |  __/ | | (_) |_|_|
+|_| |_|\___|_|_|\___/(_|_)""")
+
+
+parse_object = optparse.OptionParser()
+parse_object.add_option("-i","--interface",dest="interface",help="please enter interface name")
+parse_object.add_option("-m","--mac",dest="mac_address",help="please enter new mac address")
+
+(user_inputs,arguments) = parse_object.parse_args()
+
+#print(user_inputs.interface)
+#print(user_inputs.mac_address)
+
+user_interface = user_inputs.interface
+user_mac_address = user_inputs.mac_address
+
+#print("Macchanger Started")
+
+#interface = "eth0"
+#mac_address = "00:11:22:33:55:77"
+
+subprocess.call(["ifconfig",user_interface,"down"])
+subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_address])
+subprocess.call(["ifconfig",user_interface,"up"])
